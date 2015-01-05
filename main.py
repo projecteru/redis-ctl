732baa5c7dafbefde420b5d1fcd8049122bb3fb9
Neuron Teckid <Lene13@gmail.com>
2015-01-05 10:15:29 +0800

    stats polling

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ import handlers
 from gu import WrapperApp
 
 
+def run_app(app, debug):
+    if debug:
+        app.debug = True
+        return app.run(port=config.listen_port())
+    WrapperApp(app, {
+        'bind': '%s:%d' % ('0.0.0.0', config.listen_port()),
+        'workers': 2,
+    }).run()
+
+
 def init_app():
     conf = config.load('config.yaml' if len(sys.argv) == 1 else sys.argv[1])
     config.init_logging(conf)
@@ -15,16 +25,8 @@ def init_app():
 
     redisctl.recover.recover()
 
-    app = handlers.base.app
-    debug = conf.get('debug', 0) == 1
-    if debug:
-        app.debug = True
-        return app
-
-    return WrapperApp(app, {
-        'bind': '%s:%d' % ('0.0.0.0', config.listen_port()),
-        'workers': 2,
-    })
+    return handlers.base.app, conf.get('debug', 0) == 1
 
 if __name__ == '__main__':
-    init_app().run(port=config.listen_port())
+    app, debug = init_app()
+    run_app(app, debug)
