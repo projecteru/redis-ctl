a8e8c7bd1366347e27ccab5c056b4db5578375ab
Neuron Teckid <Lene13@gmail.com>
2015-05-12 10:14:46 +0800

    run app without gunicorn

--- main.py
+++ main.py
@@ -3,21 +3,7 @@ import models.base
 import stats.db
 
 
-def run_app(app, debug):
-    import file_ipc
-    file_ipc.write_nodes_proxies_from_db()
-    if debug:
-        app.debug = True
-        return app.run(port=config.SERVER_PORT)
-    from app import WrapperApp
-    WrapperApp(app, {
-        'bind': '0.0.0.0:%d' % config.SERVER_PORT,
-        'workers': 2,
-        'timeout': 86400,
-    }).run()
-
-
-def init_app():
+def main():
     config.init_logging()
 
     if config.INFLUXDB and config.INFLUXDB['host']:
@@ -27,8 +13,11 @@ def init_app():
     app = handlers.base.app
     app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
     models.base.init_db(app)
-    return app, config.DEBUG == 1
+
+    import file_ipc
+    file_ipc.write_nodes_proxies_from_db()
+    app.debug = config.DEBUG == 1
+    app.run(port=config.SERVER_PORT)
 
 if __name__ == '__main__':
-    app, debug = init_app()
-    run_app(app, debug)
+    main()
